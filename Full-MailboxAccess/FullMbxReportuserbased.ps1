dda99dba03487afd19d860e02d186174f3138880
sunil Chauhan <sunilkms@gmail.com>
2017-06-15 00:53:15 +0530

    Create FullMbxReportuserbased.ps1

--- Full-MailboxAccess/FullMbxReportuserbased.ps1
+++ Full-MailboxAccess/FullMbxReportuserbased.ps1
@@ -5,24 +5,29 @@ $Allmailbox = Get-Mailbox -ResultSize unlimited
 #for only Shared Mailbox
 #$Allmailbox = Get-Mailbox -ResultSize unlimited #-RecipientTypeDetails sharedMailbox 
 
+$c=0
 $MailboxPermissions=@()
 foreach($Mailbox in $AllMailbox) {
+$c++
+Write-Host "Checking($c):"$mailbox.Alias
 $prm=Get-MailboxPermission -Identity $Mailbox.alias
 $MailboxPermissions+=$prm
 }
 
+"Filtering Permission Data"
 $FilteredPermissions = $MailboxPermissions | ? {$_.User -Notlike "NT AUTHORITY*" -and `
 $_.User -Notlike "S-1-5-21*" -and $_.IsInherited -ne "True"}
 
 #select the required properties in Report
+"Preparing Report"
 $postfilter = $FilteredPermissions | select Identity,User,AccessRights
 
 "Grouping by User.."
 $Grouped = $postfilter | group User
 $Report=@()
-
-foreach ($entry in $Grouped){
+"Preparning Final Report"
+foreach ($entry in $Grouped)
+{
 $Report+=$entry.Group
 }
-
 $Report | select User,Identity, {$_.AccessRights} | Export-Csv "FullMailboxReport-Multi.csv" -NoTypeIn
