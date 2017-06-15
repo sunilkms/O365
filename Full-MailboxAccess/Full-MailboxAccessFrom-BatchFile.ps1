f20f8f30f9e234df1b5b03e63ff9034cbabd7fa6
sunil Chauhan <sunilkms@gmail.com>
2017-06-16 02:07:55 +0530

    Create Full-MailboxAccessFrom-BatchFile.ps1

---
+++ Full-MailboxAccess/Full-MailboxAccessFrom-BatchFile.ps1
@@ -0,0 +1,34 @@
+#Please Create a CSV file with the tab name Alias.
+# Alias
+# user1
+# user2
+
+$csvfile = Read-Host "Please type the path to CSV:"
+$Allmailbox = ipcsv $CsvFile
+
+$c=0
+$MailboxPermissions=@()
+foreach($Mailbox in $AllMailbox) {
+$c++
+Write-Host "Checking($c):"$mailbox.Alias
+$prm=Get-MailboxPermission -Identity $Mailbox.alias
+$MailboxPermissions+=$prm
+}
+
+"Filtering Permission Data"
+$FilteredPermissions = $MailboxPermissions | ? {$_.User -Notlike "NT AUTHORITY*" -and `
+$_.User -Notlike "S-1-5-21*" -and $_.IsInherited -ne "True"}
+
+#select the required properties in Report
+"Preparing Report"
+$postfilter = $FilteredPermissions | select Identity,User,AccessRights
+
+"Grouping by User.."
+$Grouped = $postfilter | group User
+$Report=@()
+"Preparning Final Report"
+foreach ($entry in $Grouped)
+{
+$Report+=$entry.Group
+}
+$Report | select User,Identity, {$_.AccessRights} | Export-Csv "FullMailboxReport-Multi.csv" -NoTypeIn
