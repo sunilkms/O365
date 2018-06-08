05fda82bd2ee5bd438242ee37c73c547b337617c
sunil Chauhan <sunilkms@gmail.com>
2018-06-08 18:52:00 +0530

    Update Get-CalendarLogging.ps1

--- CalendarLogging/Get-CalendarLogging.ps1
+++ CalendarLogging/Get-CalendarLogging.ps1
@@ -1,8 +1,25 @@
+# This script will fetch the logs and the run the alalysis on that and then export and ReExport.
+# Usage:
+# Get-CalendarLogging.ps1 -User "Sunil.Chauhan@domain.com" -ExportReportToPath -MeetingSubject
+
 param(
-$USER,
-$path = "D:\Sunil\CalLogs\$user",
-$subject
+$User,
+$ExportReportToPath = "D:\Sunil\CalLogs\",
+$MeetingSubject = "Test Meeting One"
 )
 
+#Below CMD will get the Diagnostics Logs and then Run Alalysis on them
 $logs = Get-CalendarDiagnosticLog -Identity $USER -Subject $subject; 
-Get-CalendarDiagnosticAnalysis -CalendarLogs $logs -DetailLevel Advanced > $subject
+# Report will be exported on the path specificed.
+$ReportPath = $ExportReportToPath +  $MeetingSubject + ".CSV"
+Get-CalendarDiagnosticAnalysis -CalendarLogs $logs -DetailLevel Advanced > $ReportPath
+
+# Export and Reimport to fix the excel formatting issue.
+
+$importedLogs = IPCSV $ReportPath
+
+#Export and Rewrite the exported File.
+$importedLogs | Export-CSV $ReportPath -NoType
+
+
+
